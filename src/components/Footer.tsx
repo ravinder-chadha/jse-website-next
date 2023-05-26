4ca88185cdc7e72fb30e8b34f3d408402e3171c3
Ravinder Chadha <ravinderchadha1233@gmail.com>
2023-05-26 16:06:07 +0530

    footer code

---
+++ src/components/Footer.tsx
@@ -0,0 +1,74 @@
+export default function Footer() {
+  return (
+    <>
+    <style jsx>{`
+      .base-image {
+        margin-top: 40px;
+        background-image: linear-gradient(rgb(255, 255, 255), rgba(4, 4, 4));
+        mask-image: linear-gradient(0deg, white, white, white, transparent);
+        -webkit-mask-image: linear-gradient(0deg,
+            white,
+            white,
+            white,
+            transparent);
+        background-color: #cccccc;
+        height: 400px;
+        background-position: 0% 28%;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }`
+      }</style>
+    <div>
+    <img src="/lib/images/Temple_above_footer.png" className="base-image w-full" alt="" />
+    <div id="footer" className="">
+    <div className="bg-[#1E1E1E] w-full flex flex-row">
+      <div className="flex basis-3/5 flex-col text-white p-10">
+        <div className="text-2xl text-wrap">
+          Sign Up for our newsletter
+        </div>
+        <div>
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+        </div>
+      </div>
+      <div className="flex basis-2/5 flex-row text-white p-10 items-center justify-around">
+        <input className="rounded-xl h-3/5 w-7/12 p-3 text-black" type="text" placeholder="Enter your email" />
+        <button className="bg-[#D62222] p-3 w-4/12 rounded-xl">Notify Me</button>
+      </div>
+      <div></div>
+    </div>
+    <div className="bg-[#1E1E1E] flex flex-row">
+      <div className="basis-1/2 p-8">
+      <img src="/lib/images/JSE_logo.png" className="w-1/3" alt="" />
+      </div>
+      <div className="basis-1/2 p-8">
+      <div className="uppercase font-bold text-[#828787] pb-4 text-[13px]">Contact Us</div>
+      <div className="flex flex-row gap-24">
+        <div>
+          <div className="font-bold text-white text-[16px]">Delhi</div>
+          <div className="text-[#828787] pt-4 pb-4 text-[16px]">White Rock, British Columbia, Canada (By Appointment Only)</div>
+          <div className="font-bold text-white text-[13px]">+91 87654789030</div>
+        </div>
+        <div>
+          <div className="font-bold text-white text-[16px]">Jalandhar</div>
+          <div className="text-[#828787] pt-4 pb-4">4th Floor, Shakti Tower, Grand trunk road, above vishal mega mart, near BMC chowk, Jalandhar, Punjab 144001</div>
+          <div className="font-bold text-white text-[13px]">+91 87654789030</div>
+        </div>
+      </div>
+      </div>
+    </div>
+    <div className="bg-[#1E1E1E] flex flex-row text-[#828787] p-4">
+      <div className="flex flex-row w-2/5 justify-around text-[11px]">
+        <p>Privacy Policy</p>
+        <p>Terms and Conditions</p>
+        <p>Tax Exemption</p>
+        <p>Jobs</p>
+      </div>
+      <div className="flex flex-row w-3/5 justify-end pr-8 text-[13px]">
+        <p>Copyright 2022-2023 - All Rights Reserved</p>
+      </div>
+    </div>
+    </div>
+    </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import CTA from '@/components/CTA'
 import Hero from '@/components/Hero'
 import UpcomingEvents from '@/components/UpcomingEvents'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <CTA />
       <UpcomingEvents />
+      <Footer />
     </main>
   )
 }
